5e8fdc765697d06df712e649e954451676e0f714
dealer426 <burnssamuel01@gmail.com>
2020-03-11 15:22:02 -0400

    Fixed api calls

--- backend/album-collection/Models/Song.cs
+++ backend/album-collection/Models/Song.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 
 namespace album_collection.Models
 {
@@ -12,6 +13,7 @@ namespace album_collection.Models
         public string Duration { get; set; }
         public string Link { get; set; }
 
+        [JsonIgnore]
         public virtual Album Album { get; set; }
         public virtual int AlbumId { get; set; }
     }
