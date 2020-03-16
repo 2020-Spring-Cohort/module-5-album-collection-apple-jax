07d6252dfef8bb621309be802230bd1a73b4c507
1robnor <robnorspeaks@gmail.com>
2020-03-16 14:27:59 -0400

    set up album controller test

--- backend/album-collection.Tests/AlbumControllerTest.cs
+++ backend/album-collection.Tests/AlbumControllerTest.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿using album_collection.Models;
+using album_collection.Repositories;
+using NSubstitute;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
+using album_collection.Controllers;
 
 namespace album_collection.Tests
 {
     public class AlbumControllerTest
     {
+        AlbumsController underTest;
+        IRepository<Album> albumRepo;
+
+        public AlbumControllerTest()
+        {
+            albumRepo = Substitute.For<IRepository<Album>>();
+            underTest = new AlbumsController(albumRepo);
+        }
+
+
+
     }
 }
