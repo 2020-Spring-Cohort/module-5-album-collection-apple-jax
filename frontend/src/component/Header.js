0a5020bff5e80c92fc31c6e0940c86a5a5b916e0
Tatyana Bailey <btatyana45@gmail.com>
2020-03-17 14:00:14 -0400

    Created files for home, header and footer. started the nav bar

---
+++ frontend/src/component/Header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+    return `<nav class='nav__header'>
+         <ul>
+             <li class='nav__home'>Home</li>
+             <li class='nav__todos'>Albums</li>
+             <li class='nav__todos'>Artists</li>
+             <li class='nav__todos'>Sons</li>
+        </ul>
+    </nav>` 
+}
