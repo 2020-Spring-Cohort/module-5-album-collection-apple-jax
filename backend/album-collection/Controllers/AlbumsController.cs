ed67d6c1c21655fbe3e04a459b0ab8e643187e8c
dealer426 <burnssamuel01@gmail.com>
2020-03-18 13:18:16 -0400

    Cleaned up Album Controller and removed comments

--- backend/album-collection/Controllers/AlbumsController.cs
+++ backend/album-collection/Controllers/AlbumsController.cs
@@ -35,15 +35,6 @@ namespace album_collection.Controllers
             return albumRepo.GetById(id);
         }
 
-        // GET: api/Albums/5
-        //[HttpGet("{id}")]
-        //public ActionResult<Album> GetAlbum(int id)
-        //{
-        //    var myAlbum = albumRepo.GetById(id);
-        //    return myAlbum;
-        //}
-
-
         // PUT: api/Albums/5
         [HttpPut("{id}")]
         public IEnumerable<Album> PutAlbum([FromBody] Album album)
@@ -51,23 +42,6 @@ namespace album_collection.Controllers
             albumRepo.Update(album);
             return albumRepo.GetAll();
         }
-
-
-        //// PUT: api/Albums/5
-        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        //// more details see https://aka.ms/RazorPagesCRUD.
-        //[HttpPut("{id}")]
-        //public IActionResult PutAlbum(int id, Album album)
-        //{
-        //    if (id != album.Id)
-        //    {
-        //        return BadRequest();
-        //    }
-
-        //    albumRepo.Update(album);
-        //    return NoContent();
-        //}
-
 
         // POST: api/Albums
         [HttpPost]
@@ -77,16 +51,6 @@ namespace album_collection.Controllers
             return albumRepo.GetAll();
         }
 
-        //// POST: api/Albums
-        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        //// more details see https://aka.ms/RazorPagesCRUD.
-        //[HttpPost]
-        //public ActionResult<Album> PostAlbum(Album album)
-        //{
-        //    albumRepo.Create(album);
-        //    return NoContent();
-        //}
-
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public IEnumerable<Album> DeleteAlbum(int id)
@@ -95,15 +59,6 @@ namespace album_collection.Controllers
             albumRepo.Delete(myAlbum);
             return albumRepo.GetAll();
         }
-
-        //// DELETE: api/Albums/5
-        //[HttpDelete("{id}")]
-        //public ActionResult<Album> DeleteAlbum(int id)
-        //{
-        //    var myAlbum = albumRepo.GetById(id);
-        //    albumRepo.Delete(myAlbum);
-        //    return NoContent();
-        //}
 
     }
 }
