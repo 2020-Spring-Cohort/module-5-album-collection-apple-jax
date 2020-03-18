3bfec5b39f4eb79da3aca923ff48e536975af548
ghassan <gh09an@gmail.com>
2020-03-18 12:20:17 -0400

    cleaned and updated code for artists

--- frontend/src/components/Artists.js
+++ frontend/src/components/Artists.js
@@ -1,21 +1,23 @@
 export default function Artists(artists){
     return `
-   
+    <h2><p><b> Artists</b></p></h2>
     ${artists.map(artist => {
         return `
         
-             <h3><p > Artist Id : ${artist.id} </p></h3>
-             <h3><p > Artist Name : ${artist.name} </p></h3>
-             <h3><p > Artist Age : ${artist.age} </p></h3>
-             <h3><p > Artist Home Town : ${artist.homeTown} </p></h3>
+             <h3><p>Name : ${artist.name} </p></h3>
+             <h3><p>Image : ${artist.image} </p></h3>
+             <img src="../../docs/img/buttons_imgtest.jpg>${artist.image} </a>
+             <h3><p>Age : ${artist.age} </p></h3>
+             <h3><p>Home Town : ${artist.homeTown} </p></h3>
+             <h3><p>Record Label : ${artist.recordLabel} </p></h3>
        
         `
     }).join("")}
     
 
-    // <section class="add-artist">
-    //         <input class="add-artist__artistName" type="text" placeholder="Add an Artist">
-    //         <button class="add-artist__submit">Add an Artist</button>
-    //     </section>
+    <section class="add-artist">
+    <input class="add-artist__artistName" type="text" placeholder="Add an Artist">
+    <button class="add-artist__submit">Add an Artist</button>
+    </section>
     `;
 }
