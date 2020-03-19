71983772dd022cf65e006bee8bc8b5cd5f3ae208
dealer426 <burnssamuel01@gmail.com>
2020-03-19 08:55:49 -0400

    Added new seed data and a new migration

--- backend/album-collection/Models/AlbumCollectionContext.cs
+++ backend/album-collection/Models/AlbumCollectionContext.cs
@@ -30,14 +30,33 @@ namespace album_collection.Models
         {
              modelBuilder.Entity<Artist>().HasData(
                  new Artist
+                 {
+                     Id = 1,
+                     Name = "Usher",
+                     Age = 25,
+                     Image = "img",
+                     RecordLabel = "columbia records",
+                     HomeTown = "atlanta",
+                 },
+                new Artist
                 {
-                    Id = 1,
-                    Name = "Usher",
+                    Id = 2,
+                    Name = "Artist A",
                     Age = 25,
                     Image = "img",
                     RecordLabel = "columbia records",
-                    HomeTown = "atlanta",
+                    HomeTown = "chicago",
+                },
+                new Artist
+                {
+                    Id = 3,
+                    Name = "Artist B",
+                    Age = 25,
+                    Image = "img",
+                    RecordLabel = "columbia records",
+                    HomeTown = "cleveland",
                 });
+
             modelBuilder.Entity<Song>().HasData(
                 new Song
                 {
@@ -46,7 +65,73 @@ namespace album_collection.Models
                     Duration = "3:12",
                     Link = "song.com",
                     AlbumId = 1,
-                });
+                },
+                new Song
+                {
+                    Id = 2,
+                    Title = "Confessions Part 2",
+                    Duration = "3:12",
+                    Link = "song.com",
+                    AlbumId = 1,
+                },
+                new Song
+                {
+                    Id = 3,
+                    Title = "Confessions Part 3",
+                    Duration = "3:12",
+                    Link = "song.com",
+                    AlbumId = 1,
+                },
+                new Song
+                {
+                    Id = 4,
+                    Title = "Song A-1",
+                    Duration = "3:12",
+                    Link = "song.com",
+                    AlbumId = 2,
+                },
+                new Song
+                {
+                    Id = 5,
+                    Title = "Song A-2",
+                    Duration = "3:12",
+                    Link = "song.com",
+                    AlbumId = 2,
+                },
+                new Song
+                {
+                    Id = 6,
+                    Title = "Song A-3",
+                    Duration = "3:12",
+                    Link = "song.com",
+                    AlbumId = 2,
+                },
+                new Song
+                {
+                    Id = 7,
+                    Title = "Song B-1",
+                    Duration = "3:12",
+                    Link = "song.com",
+                    AlbumId = 3,
+                },
+                new Song
+                {
+                    Id = 8,
+                    Title = "Song B-2",
+                    Duration = "3:12",
+                    Link = "song.com",
+                    AlbumId = 3,
+                },
+                 new Song
+                 {
+                     Id = 9,
+                     Title = "Song B-3",
+                     Duration = "3:12",
+                     Link = "song.com",
+                     AlbumId = 3,
+                 }
+
+                );
             modelBuilder.Entity<Album>().HasData(
                 new Album
                 {
@@ -55,6 +140,22 @@ namespace album_collection.Models
                     RecordLabel = "columbia records",
                     Image = "img",
                     ArtistId = 1,
+                },
+                new Album
+                {
+                    Id = 2,
+                    Title = "Artist A Album",
+                    RecordLabel = "columbia records",
+                    Image = "img",
+                    ArtistId = 2,
+                },
+                new Album
+                {
+                    Id = 3,
+                    Title = "Artist B Album",
+                    RecordLabel = "columbia records",
+                    Image = "img",
+                    ArtistId = 3,
                 });
             base.OnModelCreating(modelBuilder);
         }
