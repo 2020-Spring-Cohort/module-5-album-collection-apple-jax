6191d104b460f7c23da1ccd44f2a65abb287ed9a
ghassan <gh09an@gmail.com>
2020-03-25 07:07:30 -0400

    the style for add and delete albums page is done

--- frontend/src/components/Albums.js
+++ frontend/src/components/Albums.js
@@ -5,12 +5,11 @@ export default function Albums(albums){
     ${albums.map(album => {
         return `
         <table class="japple3"> <tr>  
-         <td>  <img src="./img/${album.image}" class="album__img"><td>
-           <td> <h3><p>Title : ${album.title}</p></h3><td>
-           <td> <h3><p>Record Label : ${album.recordLabel}</p></h3></td></tr></table>
-           <div style="text-align:right;">
-           <button class="edit-album__submit">Edit</button>
-           <button class="delete-album__submit">Delete</button></div>
+           <td><img src="./img/${album.image}" class="album__img"><td>
+           <td><h3><p>Title : ${album.title}</p></h3><td>
+           <td><h3><p>Record Label : ${album.recordLabel}</p></h3></td></tr></table>
+           <button class="edit-album__submit btn-full"> Edit</button>
+           <button class="delete-album__submit btn-full"> Delete</button>
            <input class="album__id" type="hidden" value="${album.id}">
             <br><br>
        
@@ -19,13 +18,13 @@ export default function Albums(albums){
     
     <div style="text-align:center;">
     <section class="add-album">    
-        <input class="add-album__albumTitle" type="text" placeholder="Add an album">
-        <input class="add-album__image" type="text" readonly placeholder="album1.jpg">
-        <input class="add-album__recordLabel" type="text" placeholder="Add a Record Label">
-        <input class="add-album__comments" type="text" placeholder="Add a Comment">
-        <input class="add-album__rating" type="text" placeholder="Add a Rating">
-        <input class="add-album__albumArtistId" type="text" placeholder="Add an Artist Id">
-        <button class="add-album__submit">Add an album</button>       
+        <input class="add-album__albumTitle add-album-style" type="text" placeholder="Add an album">
+        <input class="add-album__image add-album-style" type="text" readonly placeholder="album1.jpg">
+        <input class="add-album__recordLabel add-album-style" type="text" placeholder="Add a Record Label">
+        <input class="add-album__comments add-album-style" type="text" placeholder="Add a Comment">
+        <input class="add-album__rating add-album-style" type="text" placeholder="Add a Rating">
+        <input class="add-album__albumArtistId add-album-style" type="text" placeholder="Add an Artist Id">
+        <button class="add-album__submit add-album-style">Add an album</button>       
     </section>
     </div>
     
