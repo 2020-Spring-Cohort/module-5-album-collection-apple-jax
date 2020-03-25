7bcbcc2f219d9d4980c26d560c15e50a0bcd2a59
ghassan <gh09an@gmail.com>
2020-03-25 05:53:48 -0400

    style the add section in albums page

--- frontend/src/components/Albums.js
+++ frontend/src/components/Albums.js
@@ -17,16 +17,18 @@ export default function Albums(albums){
         `
     }).join("")}
     
-
-    <section class="add-album  class="japple3"">
+    <div style="text-align:center;">
+    <section class="add-album">    
         <input class="add-album__albumTitle" type="text" placeholder="Add an album">
         <input class="add-album__image" type="text" readonly placeholder="album1.jpg">
         <input class="add-album__recordLabel" type="text" placeholder="Add a Record Label">
         <input class="add-album__comments" type="text" placeholder="Add a Comment">
         <input class="add-album__rating" type="text" placeholder="Add a Rating">
         <input class="add-album__albumArtistId" type="text" placeholder="Add an Artist Id">
-        <button class="add-album__submit">Add an album</button>
+        <button class="add-album__submit">Add an album</button>       
     </section>
+    </div>
+    
     `;
 }
 
