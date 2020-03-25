e15b49b4842fdbed414a63099dbcf8a2ed70afc4
ghassan <gh09an@gmail.com>
2020-03-25 08:17:56 -0400

    the style for delete and add is done in artists page

--- frontend/src/components/Artists.js
+++ frontend/src/components/Artists.js
@@ -12,17 +12,17 @@ export default function Artists(artists){
              <button class="edit-artist__submit btn-full">Edit</button>
              <button class="delete-artist__submit btn-full">Delete</button>
              <input class="artist__id" type="hidden" value="${artist.id}">
-             </li>
+            <br><br><br>
         `
     }).join("")}
     
-
+    <div style="text-align:center;">
     <section class="add-artist">
-        <input class="add-artist__artistName" type="text" placeholder="Add an Artist">
-        <input class="add-artist__artistImage" type="text" readonly placeholder="artist1.jpg">
-        <input class="add-artist__artistAge" type="text" placeholder="Add Artist Age">
-        <input class="add-artist__artisthomeTown" type="text" placeholder="Add Home Town">
-        <input class="add-artist__artistrecordLabel" type="text" placeholder="Add Record Label">
+        <input class="add-artist__artistName add-album-style" type="text" placeholder="Add an Artist">
+        <input class="add-artist__artistImage add-album-style" type="text" readonly placeholder="artist1.jpg">
+        <input class="add-artist__artistAge add-album-style" type="text" placeholder="Add Artist Age">
+        <input class="add-artist__artisthomeTown add-album-style" type="text" placeholder="Add Home Town">
+        <input class="add-artist__artistrecordLabel add-album-style" type="text" placeholder="Add Record Label">
         <button class="add-artist__submit">Add an Artist</button>
     </section>
     `;
