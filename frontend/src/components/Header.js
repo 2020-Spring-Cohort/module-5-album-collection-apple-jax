1736c09b861ab96dfdd354018e079faec504c23f
ghassan <gh09an@gmail.com>
2020-03-24 20:38:53 -0400

    update index

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,14 @@
 export default function Header() {
     return `<nav class='nav__header'>
          <ul>
-             <li class='nav__home'>Home</li>
-             <li class='nav__albums'>Albums</li>
-             <li class='nav__artists'>Artists</li>
-             <li class='nav__songs'>Songs</li>
+         <li class='nav__home'> <h3  class="btn btn-ghost ">Home</h3></li>
+         <li class='nav__albums'> <h3  class="btn btn-ghost ">Albums</h3></li>
+         <li class='nav__artists'> <h3  class="btn btn-ghost ">Artists</h3></li>
+         <li class='nav__songs'> <h3  class="btn btn-ghost ">Songs</h3></li>
+            
         </ul>
     </nav>` 
 }
+
+
+
